e500a49bd892d28502538e6ae90d3a2efb5542b4
anusha <30501890+Anushahema@users.noreply.github.com>
2017-08-19 19:26:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-alert("anusha,welcome u to my website")l;
+alert("anusha,welcome u to my website");
 var img=document.getElementById("img");
 img.onclick=function(){
     img.style.marginLeft='100px';
