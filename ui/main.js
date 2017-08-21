fcdcc0999a8074aa541829c017cc3a66d433bda1
anusha <30501890+Anushahema@users.noreply.github.com>
2017-08-21 17:34:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,26 @@
 console.log('Loaded!');
 
-var button=document.getElementById("counter");
-var counter=0;
-button.onclick=function(){
-    var req=new XMLHTTPRequest();
+var nameinput=document.getElementByid("name");
+var fname=nameinput.value;
+var submit=document.getElementByid("submit");
+submit.onclick=function(){
+     var req=new XMLHTTPRequest();
     req.onreadystatechange=function(){
         if(req.readystate==XMLHTTPRequest.DONE)
         {
             if(req.status==200)
             {
-                var counter=req.responsetext;
-                 var span=document.getElementById("count");
-                span.innerHTML=counter.toString();
+                var name=req.responsetext;
+                name=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                     list+='<li>'+names[i]+'</li>';
+                }
             }
         }
     };
-    counter=counter+1;
-};
-req.open('GET','http://anushasakthinathan.imad.hasura-app.io/counter',true);
-req.send(null);
-
-
-var nameinput=document.getElementByid("name");
-var fname=nameinput.value;
-var submit=document.getElementByid("submit");
-submit.onclick=function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
     var ul=document.getElementByid("namelist");
     ul.innerHTML=list;
 };
+req.open('GET','http://anushasakthinathan.imad.hasura-app.io/counter',true);
+req.send(null);
