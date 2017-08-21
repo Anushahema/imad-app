129abd8c072d3a926561f08e613964a9041cfa63
anusha <30501890+Anushahema@users.noreply.github.com>
2017-08-21 17:42:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var nameinput=document.getElementByid("name");
 var fname=nameinput.value;
 var submit=document.getElementByid("submit");
 submit.onclick=function(){
-     var req=new XMLHTTPRequest();
+    var req=new XMLHTTPRequest();
     req.onreadystatechange=function(){
         if(req.readystate==XMLHTTPRequest.DONE)
         {
@@ -16,11 +16,12 @@ submit.onclick=function(){
                 for(var i=0;i<names.length;i++){
                      list+='<li>'+names[i]+'</li>';
                 }
+                var ul=document.getElementByid("namelist");
+                ul.innerHTML=list;
             }
         }
     };
-    var ul=document.getElementByid("namelist");
-    ul.innerHTML=list;
+   
 };
 req.open('GET','http://anushasakthinathan.imad.hasura-app.io/submit_name?name='+name,true);
 req.send(null);
