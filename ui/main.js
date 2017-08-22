c65e8f671892f2c917e64680cdbf97cc527a42fd
anusha <30501890+Anushahema@users.noreply.github.com>
2017-08-22 18:39:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 var button=document.getElementById("counter");
 var counter=0;
 button.onclick=function(){
-    var req=new XMLHTTPRequest();
+    var req=new XMLHttpRequest();
     req.onreadystatechange=function(){
-        if(req.readystate==XMLHTTPRequest.DONE)
+        if(req.readystate==XMLHttpRequest.DONE)
         {
            if(req.status==200)
             {
@@ -21,9 +21,9 @@ button.onclick=function(){
             
 var submit=document.getElementById("submit");
 submit.onclick=function(){
-    var request=new XMLHTTPRequest();
+    var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readystate==XMLHTTPRequest.DONE)
+        if(request.readystate==XMLHttpRequest.DONE)
         {
             if(request.status==200)
             {
